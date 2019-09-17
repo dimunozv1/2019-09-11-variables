5826d1a114e234610fba8cd30df8278621a4df0b
dimunozv1 <54986688+dimunozv1@users.noreply.github.com>
2019-09-16 23:38:03 -0500

    Add files via upload

---
+++ Tarea 1/hola.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main(void)
+
+{
+std::string x ;
+std::cout << "Esriba su nombre" << std::endl;
+std::cin >> x ;
+std::cout << "Hola, " << x << "!" << std::endl;
+return 0;
+}
---
+++ Tarea 1/mult.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(void)
+{int a = 0;
+std::cout << "Escriba un numero entero" << std::endl;
+std::cin >> a ;
+std::cout << "Su numero por dos es: " << a*2 << std::endl;
+return 0;
+}
+
